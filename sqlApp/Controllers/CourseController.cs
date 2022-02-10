991215cd8a2406d107b1a5e5bd6b6fc0473e2a8a
Ravindra Yadav <ravindra24yadav@gmail.com>
2022-02-10 16:12:35 +0530

    Add project files.

---
+++ sqlApp/Controllers/CourseController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using sqlApp.Models;
+using sqlApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sqlApp.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly CourseService _course_service;
+        private readonly IConfiguration _configuration;
+
+        public CourseController(CourseService _svc, IConfiguration _config)
+        {
+            _course_service = _svc;
+            _configuration = _config;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Course> _course_list = _course_service.GetCoures(_configuration.GetConnectionString("SQLConnection"));
+            return View(_course_list);
+        }
+    }
+}
---
+++ sqlApp/Services/CourseService.cs
@@ -0,0 +1,51 @@
+﻿using sqlApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sqlApp.Services
+{
+    public class CourseService
+    {
+       
+
+        private SqlConnection GetConnection(string _connection_string)
+        {
+          
+             return new SqlConnection(_connection_string);
+
+        }
+
+        public IEnumerable<Course> GetCoures(string _connection_string)
+        {
+            List<Course> _lst = new List<Course>();
+            string _statement = "select CourseID, CourseName, Rating from Course";
+            SqlConnection _connection = GetConnection(_connection_string);
+            _connection.Open();
+            SqlCommand _sqlcommand = new SqlCommand(_statement,_connection);
+            using(SqlDataReader _reader = _sqlcommand.ExecuteReader())
+            {
+                while(_reader.Read())
+                {
+                    Course _course = new Course()
+                    {
+                        CourseID = _reader.GetInt32(0),
+                        CourseName = _reader.GetString(1),
+                        Rating = _reader.GetDecimal(2)
+
+                    };
+
+                    _lst.Add(_course);
+                }
+
+            }
+            _connection.Close();
+            return _lst;
+        }
+
+
+
+    }
+}
